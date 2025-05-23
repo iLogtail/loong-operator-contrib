cceeb33e955c29289093e4ff7e7e29e7c594849f
gagral <keington@outlook.com>
2025-05-23 15:49:52 +0800

    修复 init 函数中的冗余代码，移除重复的 AddToScheme 调用

--- cmd/main.go
+++ cmd/main.go
@@ -50,7 +50,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 	utilruntime.Must(v1alpha1.AddToScheme(scheme))
-	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+
 	// +kubebuilder:scaffold:scheme
 }
 
